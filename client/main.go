f4a0684eb78cebaf8aa3d871163ee42ac2ff225b
Cirno <2311975835@qq.com>
2023-08-05 15:52:41 +0800

    rpc

--- client/main.go
+++ client/main.go
@@ -5,12 +5,17 @@ import (
 	"context"
 	"fmt"
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/credentials"
 	"log"
 )
 
 func main() {
-	conn, err := grpc.Dial(":8000", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	file1, err := credentials.NewClientTLSFromFile("client/cert_client/server.pem", "cirno.com")
+	if err != nil {
+		log.Fatal("证书错误 ", err)
+	}
+
+	conn, err := grpc.Dial(":8000", grpc.WithTransportCredentials(file1))
 	if err != nil {
 		log.Fatal(err)
 	}
--- main.go
+++ main.go
@@ -4,13 +4,20 @@ import (
 	"GRPC_TEST/service"
 	"fmt"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
 	"log"
 	"net"
 )
 
 func main() {
+	//添加证书
+	file1, err := credentials.NewServerTLSFromFile("cert/server.pem", "cert/server.key")
+	if err != nil {
+		log.Fatal(err)
+	}
 	fmt.Printf("Test")
-	rpcServer := grpc.NewServer()
+	rpcServer := grpc.NewServer(grpc.Creds(file1))
+
 	service.RegisterProdServiceServer(rpcServer, &service.ProduceService{})
 	listener, err := net.Listen("tcp", ":8000")
 	if err != nil {
