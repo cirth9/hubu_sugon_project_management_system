b90641b2ebed497f536da902de2d27d363409a6b
Cirno <2311975835@qq.com>
2023-08-07 13:22:17 +0800

    RPC

--- client/main.go
+++ client/main.go
@@ -3,33 +3,56 @@ package main
 import (
 	"GRPC_TEST/service"
 	"context"
+	"crypto/tls"
+	"crypto/x509"
 	"fmt"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials"
 	"log"
+	"os"
 )
 
 func main() {
-	file1, err := credentials.NewClientTLSFromFile("client/cert_client/server.pem", "cirno.com")
+	//file1, err := credentials.NewClientTLSFromFile("client/cert_client/server.pem", "cirno.com")
+	//if err != nil {
+	//	log.Fatal("证书错误 ", err)
+	//}
+	file, err := tls.LoadX509KeyPair("cert/client.pem", "cert/client.key")
 	if err != nil {
-		log.Fatal("证书错误 ", err)
+		log.Fatal(1, err)
 	}
-
-	conn, err := grpc.Dial(":8000", grpc.WithTransportCredentials(file1))
+	certPool := x509.NewCertPool()
+	ca, err := os.ReadFile("cert/ca.crt")
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal(2, err)
+	}
+	certPool.AppendCertsFromPEM(ca)
+	creds := credentials.NewTLS(&tls.Config{
+		// 设置证书链，允许包含一个或多个
+		Certificates: []tls.Certificate{file},
+		// 要求必须校验客户端的证书。可以根据实际情况选用以下参数
+		ServerName: "*.cirnoblog.cn",
+		RootCAs:    certPool,
+	})
+
+	conn, err := grpc.Dial(":8002", grpc.WithTransportCredentials(creds))
+	if err != nil {
+		log.Fatal(3, err)
 	}
 	defer func(conn *grpc.ClientConn) {
 		err = conn.Close()
 		if err != nil {
-			log.Fatal(err)
+			log.Fatal(4, err)
 		}
 	}(conn)
 
 	rpcClient := service.NewProdServiceClient(conn)
-	resp, err := rpcClient.GetProdStock(context.Background(), &service.Product_Request{})
+	request := &service.Product_Request{
+		ProdId: 123,
+	}
+	resp, err := rpcClient.GetProdStock(context.Background(), request)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal("response failed ", err)
 	}
 	fmt.Printf("%#v", resp)
 }
--- main.go
+++ main.go
@@ -2,24 +2,51 @@ package main
 
 import (
 	"GRPC_TEST/service"
+	"crypto/tls"
+	"crypto/x509"
 	"fmt"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials"
 	"log"
 	"net"
+	"os"
 )
 
 func main() {
 	//添加证书
-	file1, err := credentials.NewServerTLSFromFile("cert/server.pem", "cert/server.key")
+	//file1, err := credentials.NewServerTLSFromFile("cert/server.pem", "cert/server.key")
+	//if err != nil {
+	//	log.Fatal(err)
+	//}
+
+	//证书认证，双向认证
+	//从证书相关文件中读取和解析信息，得到证书的公钥秘钥对
+	file, err := tls.LoadX509KeyPair("cert/server.pem", "cert/server.key")
 	if err != nil {
 		log.Fatal(err)
 	}
+	//创建certPool（证书池）
+	certPool := x509.NewCertPool()
+	ca, err := os.ReadFile("cert/ca.crt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	//尝试解析所传入的 PEM 编码的证书。如果解析成功会将其加到 CertPool 中，便于后面的使用
+	certPool.AppendCertsFromPEM(ca)
+	// 构建基于 TLS 的 TransportCredentials 选项
+	newTLS := credentials.NewTLS(&tls.Config{
+		// 设置证书链，允许包含一个或多个
+		Certificates: []tls.Certificate{file},
+		// 要求必须校验客户端的证书。可以根据实际情况选用以下参数
+		ClientAuth: tls.RequireAndVerifyClientCert,
+		// 设置根证书的集合，校验方式使用 ClientAuth 中设定的模式
+		ClientCAs: certPool,
+	})
 	fmt.Printf("Test")
-	rpcServer := grpc.NewServer(grpc.Creds(file1))
+	rpcServer := grpc.NewServer(grpc.Creds(newTLS))
 
 	service.RegisterProdServiceServer(rpcServer, &service.ProduceService{})
-	listener, err := net.Listen("tcp", ":8000")
+	listener, err := net.Listen("tcp", ":8002")
 	if err != nil {
 		log.Fatal(err)
 	}
