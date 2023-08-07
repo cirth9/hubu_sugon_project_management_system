639916d4fcca1faf447099e07b9bb02de22e06be
Cirno <2311975835@qq.com>
2023-08-08 01:43:23 +0800

    rpc

---
+++ client/auth/auth.go
@@ -0,0 +1,21 @@
+package auth
+
+import (
+	"context"
+	_ "google.golang.org/grpc/credentials"
+)
+
+type Authentication struct {
+	User     string
+	Password string
+}
+
+func (a *Authentication) GetRequestMetadata(context.Context, ...string) (
+	map[string]string, error,
+) {
+	return map[string]string{"user": a.User, "password": a.Password}, nil
+}
+
+func (a *Authentication) RequireTransportSecurity() bool {
+	return false
+}
--- client/main.go
+++ client/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"GRPC_TEST/client/auth"
 	"GRPC_TEST/service"
 	"context"
 	"crypto/tls"
@@ -12,11 +13,25 @@ import (
 	"os"
 )
 
+var (
+	username string //用户名
+	password string //密码
+)
+
 func main() {
 	//file1, err := credentials.NewClientTLSFromFile("client/cert_client/server.pem", "cirno.com")
 	//if err != nil {
 	//	log.Fatal("证书错误 ", err)
 	//}
+	_, err := fmt.Scan(&username, &password)
+	if err != nil {
+		log.Fatal("Input error")
+	}
+	user := &auth.Authentication{
+		User:     username,
+		Password: password,
+	} //username 和 password都错了，预计输出password or username wrong
+
 	file, err := tls.LoadX509KeyPair("cert/client.pem", "cert/client.key")
 	if err != nil {
 		log.Fatal(1, err)
@@ -27,7 +42,7 @@ func main() {
 		log.Fatal(2, err)
 	}
 	certPool.AppendCertsFromPEM(ca)
-	creds := credentials.NewTLS(&tls.Config{
+	newTLS := credentials.NewTLS(&tls.Config{
 		// 设置证书链，允许包含一个或多个
 		Certificates: []tls.Certificate{file},
 		// 要求必须校验客户端的证书。可以根据实际情况选用以下参数
@@ -35,7 +50,8 @@ func main() {
 		RootCAs:    certPool,
 	})
 
-	conn, err := grpc.Dial(":8002", grpc.WithTransportCredentials(creds))
+	//Dial第一个参数用于处理双向认证，第二个参数用于检测token是否合法，在此处指user和password是否合法
+	conn, err := grpc.Dial(":8002", grpc.WithTransportCredentials(newTLS), grpc.WithPerRPCCredentials(user))
 	if err != nil {
 		log.Fatal(3, err)
 	}
